18446947bf442c1c84aa5edc910f7a616fc10c28
Abdurrahmaan9 <abdurrahmaanchimalo5@gmail.com>
2023-12-13 08:50:43 +0200

    putting colors at random to our board of the game

--- Candy_Crush/scripts.js
+++ Candy_Crush/scripts.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const grid = document.querySelector('.grid')
+    const width = 8
+    const divs = []
+
+
+    const candy_colors = [
+        'red',
+        'blue',
+        'green',
+        'yellow',
+        'orange',
+        'purple'
+    ]
+    // create Board
+    function create_board() {
+        for (let i = 0; i < width*width; i++) {
+            const div = document.createElement('div');
+            let random_color = Math.floor(Math.random() * candy_colors.length)
+            div.style.backgroundColor = candy_colors[random_color]
+            grid.appendChild(div);
+            divs.push(div)
+        }
+    }
+
+create_board()
+
 })
